ea3c7580df89f132145d0abf16067308011b6367
Nozim Mehrubonov <nozim@zumata.com>
2016-03-18 15:23:06 +0800

    Making path to data file and uids as command line args

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 
 0. Make sure you have java 1.8 JRE and JDK installed
 1. Navigate into project root directory
-2. Run `activator run`
+2. Run `./activator 'run [data file] uid1 uid2'`
 
 ## Definition of "meeting"
 I'm assuming that both persons were within a distance of 3 meters in 1 minute time frame
--- src/main/scala/com/m8vn3m/FindMeetingsApp.scala
+++ src/main/scala/com/m8vn3m/FindMeetingsApp.scala
@@ -6,9 +6,9 @@ object FindMeetingsApp extends SearchByRoutes {
   def maxDistance = 3
 
   def main(args: Array[String]): Unit = {
-    val path = "./data/reduced.csv"
-    val person1ID = "5a8ace07"
-    val person2ID = "a7a5f9e5"
+    val path = args(0)
+    val person1ID = args(1)
+    val person2ID = args(2)
 
     val dataSource : DataSource = new CSVDataSource(path)
     val meetings = search(dataSource.fetch(),person1ID,person2ID)
